8634be1d0672f1326254ab53efbf85ad9b24be50
fernandogonzalez11 <fernandogonzalezeducativo@gmail.com>
2021-03-04 22:05:07 -0600

    Módulo 2 Añadí los archivos del módulo 2

---
+++ Módulo 2/declaracionYTipos.js
@@ -0,0 +1,58 @@
+// Programa número 1 en JavaScript
+// Declaración de variable
+let numero = 365; 
+console.log(numero);
+numero = 5000;
+console.log(numero);
+
+// Declaración de constante
+const mensaje = "Bienvenido a Ciudad Río Verde";
+//mensaje = "Bienvenido a Ciudad Río Claro";
+console.log(mensaje);
+
+// Características de una persona
+const nombre = "Fernando",
+    apellidoPaterno = "González",
+    apellidoMaterno = "Robles",
+    lugarNacimiento = "Costa Rica";
+let edad = 14;
+
+// Características de un carro
+const carroNombre = "Mustang",
+    placa = "BCI-298", // Las placas no se modifican en Costa Rica
+    marca = "Ford",
+    fabricacion = 1964;
+let precio = 2368; // Dólares en 1964
+
+// Number
+const numero1 = 5,
+    numero2 = 1500;
+// String
+const texto1 = "Bicicleta",
+    texto2 = "Bienvenido";
+// Boolean
+const booleanoVerdadero = true;
+const booleanoFalso = false;
+// Null & Undefined
+let condicion = null;
+let sinValor; // No declarar es undefined
+// Array
+const arregloNumeros = [5000, 4200, 10, 30, 15, 10000];
+const arregloNombres = ["Luis", "Erick", "Diego", "Ana"];
+const arregloMixto = [5000, "Luis", 10, "Erick", 15, "Diego"];
+// Object - Persona
+const persona = {
+    nombre: "Fernando",
+    apellidoPaterno: "González",
+    apellidoMaterno: "Robles",
+    edad: 14,
+    lugarNacimiento: "Costa Rica"
+};
+// Object - Carro
+const carro = {
+    nombre: "Mustang",
+    marca: "Ford",
+    placa: "BCI-298",
+    fabricacion: 1964,
+    precio: 2368
+};
---
+++ Módulo 2/operadores.js
@@ -0,0 +1,115 @@
+// Usar chalk
+const { blue } = require("chalk");
+// Acortar el console.log() a solo log()
+const { log } = console;
+
+// Declarar operandos
+let op1 = 10, op2 = 4, op3 = 300, op4 = 15;
+
+/* OPERADORES ARITMÉTICOS */
+log(blue("OPERADORES ARITMÉTICOS"));
+
+// Adición ( + )
+let res = op1 + op2;
+log("Adición:", res);
+
+// Substracción ( - )
+res = op1 - op2;
+log("Substracción:", res);
+
+// Multiplicación ( * )
+res = op1 * op2;
+log("Multiplicación:", res);
+
+// División ( / )
+res = op3 / op4;
+log("División:", res);
+
+// Exponente ( ** )
+res = op1 ** op2; // Math.pow(op1, op2)
+log("Exponente:", res);
+
+// Módulo ( % )
+res = op1 % op2;
+log("Módulo:", res);
+
+// Incremento ( ++ )
+res = op1++;
+log("Incremento:", res);
+
+// Decremento ( -- )
+res = op1--;
+log("Decremento:", res);
+
+
+/* OPERADORES DE ASIGNACIÓN */
+log("\n" + blue("OPERADORES DE ASIGNACIÓN"));
+
+// Asignación normal ( = )
+op1 = 7;
+log("op1 ahora es:", op1);
+
+// Asignación con adición ( += )
+op1 += 3;
+log("op1 más 3:", op1);
+
+// Asignación con substracción ( -= )
+op1 -= 4;
+log("op1 menos 4:", op1);
+
+// Asignación con multiplicación ( *= )
+op1 *= 2;
+log("op1 por dos:", op1);
+
+// Asignación con división ( /= )
+op1 /= 3;
+log("op1 entre tres:", op1);
+
+// Asignación con módulo ( %= )
+op1 %= 3;
+log("módulo de op1 con 3:", op1);
+ 
+
+/* OPERADORES LÓGICOS */
+log("\n" + blue("OPERADORES LÓGICOS"));
+op1 = 5, op2 = 5, op3 = 10, op4 = "5";
+
+// Igual que ( == )             // valor
+res = op1 == op2; // 1
+log("5 == 5:", res);
+res = op1 == op3; // 2
+log("5 == 10:", res);
+res = op1 == op4; // 3
+log("5 == \"5\":", res);
+
+// Igual que estricto ( === )   // valor y tipo
+res = op1 === op2; // 1
+log("5 === 5:", res);
+res = op1 === op3; // 2
+log("5 === 10:", res);
+res = op1 === op4; // 3
+log("5 === \"5\"", res);
+
+// Diferente que ( != )         // valor
+res = op1 != op2; // 1
+log("5 != 5:", res);
+
+// Diferente que estricto (!==) // valor y tipo
+res = op1 !== op2; // 1
+log("5 !== 5:", res);
+
+// Mayor que ( > )
+res = op1 > op3;
+log("5 < 10:", res);
+
+// Menor que ( < )
+res = op1 < op3;
+log("5 > 10:", res);
+
+// Mayor o igual que ( >= )
+res = op1 >= op3;
+log("5 >= 10:", res);
+
+// Menor o igual que ( <= )
+res = op1 <= op3;
+log("5 <= 10:", res);
