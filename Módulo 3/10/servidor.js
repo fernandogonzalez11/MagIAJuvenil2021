a0a03d05dc651b811d18b950aa7875510abcb7eb
fernandogonzalez11 <fernandogonzalezeducativo@gmail.com>
2021-04-22 13:29:11 -0600

    Patch: servidor.js Quitar los headers iniciales y ponerlos después, para poder usar el header de text/html también

--- Módulo 3/10/servidor.js
+++ Módulo 3/10/servidor.js
@@ -1,29 +1,34 @@
 const http = require('http');
 const chalk = require('chalk');
+const webData = require('fs').readFileSync('./web.html', { encoding: 'utf-8' });
 
 const host = 'localhost',
     port = '8080';
 
+/**
+ * @param {http.IncomingMessage} req 
+ * @param {http.ServerResponse} res */
 function servicioMensaje(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    
-    /*res.writeHead(200);
-    res.end(JSON.stringify({
-        mensaje: 'hoalholaholaohloalh'
-    }));*/
     switch(req.url) {
         case '/saludo':
         case '/':
+            res.setHeader('Content-Type', 'application/json');
             res.writeHead(200);
             res.end(JSON.stringify({
                 mensaje: 'hoalholaholaohloalh'
             }));
             break;
         case '/mensajeNavidad':
+            res.setHeader('Content-Type', 'application/json');
             res.writeHead(200);
             res.end(JSON.stringify({
                 mensaje: 'feliz navidad bro que la pases bien'
             }));
+            break;
+        case '/pagina':
+            res.setHeader('Content-Type', 'text/html');
+            res.writeHead(200);
+            res.end(webData);
             break;
         default:
             res.writeHead(400);
